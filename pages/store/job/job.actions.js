4a9e1735f3f92ce7cafd2ce3d248edf042b0b89b
Zuhair Naqi <zuhair.naqi@iomechs.com>
2020-12-08 12:37:06 +0500

    Move searching algorithm to epic

--- pages/store/job/job.actions.js
+++ pages/store/job/job.actions.js
@@ -1,5 +1,7 @@
 export const SET_JOB_DATA = 'SET_JOB_DATA';
-export const SET_JOB_DATA_SUCCESSFULLY = 'SET_JOB_DATA_SUCCESSFULLY';
+export const SET_JOB_DATA_SUCCESSFULL = 'SET_JOB_DATA_SUCCESSFULL';
+export const SET_JOB_FILTERED_DATA = 'SET_JOB_FILTERED_DATA';
+export const SET_JOB_FILTERED_DATA_SUCCESSFULL = 'SET_JOB_FILTERED_DATA_SUCCESSFULL';
 
 export const setJobData = data => ({
   type: SET_JOB_DATA,
@@ -7,7 +9,17 @@ export const setJobData = data => ({
 });
 
 export const setJobDataSuccess = data => ({
-  type: SET_JOB_DATA_SUCCESSFULLY,
+  type: SET_JOB_DATA_SUCCESSFULL,
+  payload: data,
+});
+
+export const setJobFilteredData = searchText => ({
+  type: SET_JOB_FILTERED_DATA,
+  payload: searchText,
+});
+
+export const setJobFilteredDataSuccess = data => ({
+  type: SET_JOB_FILTERED_DATA_SUCCESSFULL,
   payload: data,
 });
 
