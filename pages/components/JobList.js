0af962b6f26fd4a9bd2607c2193af53dbe99e667
syedsaadqamar <syed.saad@iomechs.com>
2020-12-08 18:31:36 +0500

    Add asc and desc sortBy

--- pages/components/JobList.js
+++ pages/components/JobList.js
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
 import styles from './../../styles/Home.module.css';
 import Card from 'react-bootstrap/Card';
-import { Accordion, Button, Badge } from 'react-bootstrap';
+import { Accordion, Button, Badge, OverlayTrigger, Popover } from 'react-bootstrap';
 import DepartmentModal from './DepartmentModal';
 import { connect } from 'react-redux';
 
-const sortBy = [
+const sortByData = [
   {
     key: 'Location',
     filterBy: ''
@@ -28,6 +28,8 @@ const sortBy = [
   },
 ]
 const JobList = ({ filteredList, jobsFilteredData }) => {
+  const [sortBy, setSortBy] = useState(sortByData);
+  const [jobsCount, setJobsCount] = useState([]);
   const [jobs, changeJobs] = useState([]);
   const [modalShow, setModalShow] = useState(false);
   const [selectedItem, setSelectedItem] = useState({department: []});
@@ -36,6 +38,15 @@ const JobList = ({ filteredList, jobsFilteredData }) => {
     changeJobs(jobsFilteredData);
   }, [jobsFilteredData]);
 
+
+  useEffect(() => {
+    let jobSum = 0;
+    for (const key in filteredList) {
+      jobSum += filteredList[key].map(filter => filter.doc_count).reduce((xDocCount, yDocCount) => xDocCount + yDocCount, 0);
+    }
+    setJobsCount(jobSum);
+  }, [filteredList]);
+
   const avatarName = (name) => {
     return name.split(" ").slice(0, 2).map(str => str[0]).join("");
   };
@@ -43,22 +54,56 @@ const JobList = ({ filteredList, jobsFilteredData }) => {
   const openModal = (item) => {
     setSelectedItem(item);
     setModalShow(true);
+  };
+
+  const closeAllFilter = () => {
+    return [...sortBy].map(sort => {
+      sort.show = false;
+      return sort;
+    });
   }
+
+  const handleClick = (index) => {
+    const updatedSort = closeAllFilter();
+    updatedSort[index].show = !updatedSort[index].show;
+    setSortBy([...updatedSort]);
+  };
+
+  const selectFilterSortBy = (type, index) => {
+    const updatedSort = closeAllFilter();
+    updatedSort[index].sortByVal = type;
+    setSortBy([...updatedSort]);
+  }
+
   return (
     <div className={styles.jobList}>
-      {/* <div>
-        <span></span>
-        <>
-          <span>Sort by</span>
-          <div style={{display: 'flex'}}>
+      <div style={{display: 'flex', justifyContent: 'space-between', padding: '40px 10px'}}>
+        <span><b>{jobsCount}</b> job postings</span>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <span style={{color: '#ccc'}}>Sort by</span>
             {
-              sortBy.map((value, index) => (
-                <span key={index}>{value}</span>
+              sortBy.map((val, index) => (
+                <OverlayTrigger
+                    trigger="click"
+                    show={val.show}
+                    key={index+1}
+                    placement={'bottom'}
+                    overlay={
+                      <Popover id={index}>
+                        <Popover.Content>
+                          <div style={{cursor: 'pointer'}} onClick={() => selectFilterSortBy('asc', index)}>Asscending</div>
+                          <div style={{cursor: 'pointer'}} onClick={() => selectFilterSortBy('desc', index)}>Decending {val.sortByVal === 'desc' && <span>check</span>}</div>
+                          <div style={{cursor: 'pointer'}} onClick={() => selectFilterSortBy('', index)}>Remove</div>
+                        </Popover.Content>
+                      </Popover>
+                    }
+                  >
+                  <span onClick={() => handleClick(index)} style={{marginLeft: '12px', cursor: 'pointer'}}>{val.key}{val.sortByVal && <span style={{color: '#ccc'}}>{':' + val.sortByVal}</span>}</span>
+                </OverlayTrigger>
               ))
             }
-          </div>
-        </>
-      </div> */}
+        </div>
+      </div>
       {
         jobs.map((job, pIndex) => (
           <Accordion key={pIndex}>
